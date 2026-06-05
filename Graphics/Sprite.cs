202ce21b6ef4eecfe60ed683d8d2ed7ed9e44eff
konkeydong <dittoes.1@gmail.com>
2026-06-04 20:18:48 -0500

    remove dead sprite draw function

--- Graphics/Sprite.cs
+++ Graphics/Sprite.cs
@@ -16,30 +16,4 @@ public class Sprite
         Texture = SpriteManager.Load(path);
         FrameRect = frameRect;
     }
-
-    public void Draw(Vector2 position, float scale)
-    {
-        var source = new Rectangle(
-            x: FrameRect.x,
-            y: FrameRect.y,
-            width: FrameRect.w,
-            height: FrameRect.h
-        );
-
-        var dest = new Rectangle(
-            x: position.X,
-            y: position.Y,
-            width: FrameRect.w * scale,
-            height: FrameRect.h * scale
-        );
-
-        Raylib.DrawTexturePro(
-            Texture,
-            source,
-            dest,
-            GameConfig.Textures.BaseOrigin,
-            GameConfig.Textures.BaseRotation,
-            GameConfig.Textures.ClearColor
-        );
-    }
 }
