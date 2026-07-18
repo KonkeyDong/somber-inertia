0a5e6f74d68471fb1742c47305538c3cd6248d05
konkeydong <dittoes.1@gmail.com>
2026-07-17 21:50:17 -0500

    more constant renaming

--- State/BattleResolution.cs
+++ State/BattleResolution.cs
@@ -63,8 +63,8 @@ public class BattleResolution : IGameState
 
     public void Draw(float scale)
     {
-        var backgroundPosition = GameConstants.Battle.Positions.BASE_BACKGROUND_POSITION * scale;
-        var foregroundPosition = GameConstants.Battle.Positions.BASE_FOREGROUND_POSITION * scale;
+        var backgroundPosition = GameConstants.Battle.Positions.Background * scale;
+        var foregroundPosition = GameConstants.Battle.Positions.Foreground * scale;
         var unfriendlyPosition = GameConstants.BASE_UNFRIENDLY_POSITION * scale;
         var friendlyPosition   = _game.AttackContext.ForceMemberSpriteSet.BasePosition * scale;
         var unfriendlyStatsPosition = GameConstants.Battle.Positions.UnfriendlyStats * scale;
