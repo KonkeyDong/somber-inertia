a26153cba6f93fddba5418d8e85801d88c6f8f79
konkeydong <dittoes.1@gmail.com>
2026-08-15 14:57:37 -0500

    add promoted max sword of darkness sprites

--- Constants/GameConstants.cs
+++ Constants/GameConstants.cs
@@ -202,6 +202,7 @@ public static class GameConstants
             { $"{UnitName.Max.GetBaseName()}_{promoted}_{ItemName.SteelSword.GetBaseName()}", new Vector2(105, 80) },
             { $"{UnitName.Max.GetBaseName()}_{promoted}_{ItemName.BroadSword.GetBaseName()}", new Vector2(105, 80) },
             { $"{UnitName.Max.GetBaseName()}_{promoted}_{ItemName.DoomBlade.GetBaseName()}", new Vector2(105, 80) },
+            { $"{UnitName.Max.GetBaseName()}_{promoted}_{ItemName.SwordOfDarkness.GetBaseName()}", new Vector2(101, 78) },
 
             // Anri
             { $"{UnitName.Anri.GetBaseName()}_{unpromoted}_{ItemName.Unarmed.GetBaseName()}", new Vector2(150, 85) },
