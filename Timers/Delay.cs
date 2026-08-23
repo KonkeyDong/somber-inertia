1b9af87cd1d1df973775e79e7d6d3e7bc709fb00
konkeydong <dittoes.1@gmail.com>
2026-08-23 13:51:24 -0500

    add start delay after n frames to delay timer

--- Timers/Delay.cs
+++ Timers/Delay.cs
@@ -5,23 +5,52 @@ namespace SomberInertia.Timers;
 /// </summary>
 public class Delay : ITimer
 {
-    private readonly int _delayFrames;
+    private int _tickDelayAmount;
+    private readonly int _originalTickDelayAmount;
+    private int _startDelayFrames;
     private int _currentTick;
     private int _currentIndex;
 
     public int CurrentIndex => _currentIndex;
 
-    public Delay(int delayFrames)
+    public Delay(int tickDelayAmount)
     {
-        _delayFrames = delayFrames;
+        _tickDelayAmount = tickDelayAmount;
+        _startDelayFrames = 0;
+        _originalTickDelayAmount = 0;
+        Reset();
+    }
+
+    // Wait N number of frames before starting the timer. 
+    // Useful when you have an array of Delay timers that all
+    // need to start their timers at different times.
+    public Delay(int tickDelayAmount, int startDelayFrames)
+    {
+        if (startDelayFrames < 0)
+        {
+            Logger.Error("Delay timer cannot have its StartDelayFrames amount be less than zero.");
+        }
+
+        _tickDelayAmount = tickDelayAmount;
+        _startDelayFrames = startDelayFrames;
+        _originalTickDelayAmount = startDelayFrames;
+
         Reset();
     }
 
     public void Tick()
     {
+        // do not start the timer until the initial
+        // _startDelayFrames have reached zero.
+        if (_startDelayFrames > 0)
+        {
+            _startDelayFrames--;
+            return;
+        }
+
         _currentTick++;
 
-        if (_currentTick >= _delayFrames)
+        if (_currentTick >= _tickDelayAmount)
         {
             _currentIndex++;
             _currentTick = 0;
@@ -32,10 +61,12 @@ public class Delay : ITimer
     {
         _currentTick = 0;
         _currentIndex = 0;
+        _startDelayFrames = _originalTickDelayAmount;
     }
 
     public void ResetTimerOnly()
     {
         _currentTick = 0;
+        _startDelayFrames = _originalTickDelayAmount;
     }
 }
