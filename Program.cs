5d9a0e615a7bea401c6e7d9a2e7c953a53e4cbc2
konkeydong <dittoes.1@gmail.com>
2026-09-04 21:13:19 -0500

    add gargoyle sprites

--- Program.cs
+++ Program.cs
@@ -112,7 +112,7 @@ class Program
         tao.AddItem(ItemName.WoodenStaff, autoEquipWeapon: false);
         tao.AddItem(ItemName.PowerStaff, autoEquipWeapon: false);
 
-        var goblin1 = new Unit(UnitName.EvilPuppet);
+        var goblin1 = new Unit(UnitName.Gargoyle);
         goblin1.Defense = 5;
         goblin1.HP.Current = 1;
 
